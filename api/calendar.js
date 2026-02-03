bfb9dfe0320043aedb29316fbd081909bfc72897
ABNER2004CP <warior.rhm@gmail.com>
2026-02-03 11:02:28 +0330

    Update calendar.js

--- api/calendar.js
+++ api/calendar.js
@@ -4,14 +4,12 @@ export default async function handler(req, res) {
   try {
     const url = "https://www.forexfactory.com/calendar";
     const html = await fetch(url).then(r => r.text());
-
     const $ = cheerio.load(html);
-    const rows = $("tr.calendar__row");
 
     const events = [];
 
-    rows.each((i, row) => {
-      const time = $(row).find(".calendar__time").text().trim();
+    $("tr.calendar__row, tr.calendar__row--all-day").each((i, row) => {
+      const time = $(row).find(".calendar__time").text().trim() || "All Day";
       const currency = $(row).find(".calendar__currency").text().trim();
       const impact = $(row).find(".calendar__impact span").attr("title") || "";
       const event = $(row).find(".calendar__event").text().trim();
